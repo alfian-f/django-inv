7fbc7a63ec27e643aa02032099fd5d841981c16d
alfian-f <alfian.0615@gmail.com>
2023-09-26 16:42:43 +0700

    Assignment 4

--- main/models.py
+++ main/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Item(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     date_added = models.DateField(auto_now_add=True)
     amount = models.IntegerField()
     description = models.TextField()
-    category = models.TextField()
+    category = models.CharField(max_length=100)
     price = models.IntegerField()
